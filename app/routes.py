0bf6a3adec3ebb54dd952403452b4f477eaae7e7
Mahir Chavda <mahir.chavda13@gmail.com>
2021-06-23 17:24:24 +0530

    Update endpoint

--- app/routes.py
+++ app/routes.py
@@ -8,14 +8,14 @@ def index():
     return """<title>scripts - mahirchavda</title><style>.fun{font-family:cursive;font-style: italic;font-weight:bold;font-size:xx-large;}</style><center style="margin-top:47px;" class="fun">Hi, I'm mahirchavda</center>"""
 
 
-@app.route("/script/<script_name>")
+@app.route("/<script_name>")
 def get_script(script_name):
     try:
         return render_template(script_name, data=request.args, script_name=script_name)
     except TemplateNotFound as ex:
         abort(
             404,
-            description='"Unknown {script_name}" script!!'.format(
+            description='Unknown script: "{script_name}"'.format(
                 script_name=script_name
             ),
         )
