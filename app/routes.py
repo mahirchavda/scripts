17870d6c47c182145718d6a9ddd60a6cd7de17fd
Mahir Chavda <mahir.chavda13@gmail.com>
2021-07-18 21:51:33 +0530

    Update home endpoint

--- app/routes.py
+++ app/routes.py
@@ -5,7 +5,7 @@ from app import app
 
 @app.route("/")
 def index():
-    return """<title>scripts - mahirchavda</title><style>.fun{font-family:cursive;font-style: italic;font-weight:bold;font-size:xx-large;}</style><center style="margin-top:47px;" class="fun">Hi, I'm mahirchavda</center>"""
+    return render_template("public/index.html")
 
 
 @app.route("/<script_name>")
